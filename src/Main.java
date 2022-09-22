ba53af52ab09745aca0a83c6d00841a38eddcf0f
Дом <ira-rostov80@mail.ru>
2022-09-22 13:05:57 +0300

    Metod september

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Metod number");
+
+        System.out.println("Задача 1");
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + ",");
+        }
+    }
+         System.out.println("Задача 2");
+         double [] numb = {1.57, 7.654, 9.986};
+         for (double i=0; i < numb.length; i++){
+         Sysrem.out.print (numb [(int) i]+ ",");
+        }
     }
 
-          System.out.println("Задача 1");
-          int [] number = new int[3];
-          number [0] = 1;
-          number [1] = 2;
-          number [2] = 3;
-          for (int i=0; i < number.length; i++){
-        System.out.print(number[1]+",");
+        System.out.println("Задача 3");
+        int [] numb1 = {3, 5, 18, 23, 34, 41, 54};
+        for (int i=0; i < numd1.length; i++);
+        System.out.print(numb1 [i] + ",");
+        System.out.println();
         }
+    }
