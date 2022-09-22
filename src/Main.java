c9a3568fdfc5f799868296c2a0a60d02f5dc3c35
Дом <ira-rostov80@mail.ru>
2022-09-22 11:26:53 +0300

    Metod september

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Metod number");
+    }
+}
+          System.out.println("Задача 1");
+          int [] number = new int[3];
+          number [0] = 1;
+          number [1] = 2;
+          number [2] = 3;
+          for (int i=0; i < number.length; i++){
+        System.out.print(number[1]+",");
+        }
+        }
